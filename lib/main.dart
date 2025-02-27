1e18f8855bb7494fb1f0af84fa5b153de489feac
itsPragadees <pragadeesofficialit@gmail.com>
2025-02-27 18:14:47 +0530

    Material App added

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,18 @@
-import 'package:fitness_mobile_app/screens/onboarding_one.dart';
+import 'package:fitness_mobile_app/screens/welcome_screen_one.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const FitnessMobileApp());
 }
 
-class FitnessMobileApp extends StatefulWidget {
+class FitnessMobileApp extends StatelessWidget {
   const FitnessMobileApp({super.key});
 
   @override
-  State<FitnessMobileApp> createState() => _FitnessMobileAppState();
-}
-
-class _FitnessMobileAppState extends State<FitnessMobileApp> {
-  @override
   Widget build(BuildContext context) {
-    
-    return const OnboardingOne();
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: WelcomeScreenOne(),
+    );
   }
 }

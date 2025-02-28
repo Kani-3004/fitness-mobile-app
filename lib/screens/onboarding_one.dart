d1ad4a193136d67b04b6dd31cea9008c9c93448e
itsPragadees <pragadeesofficialit@gmail.com>
2025-02-28 13:05:34 +0530

    Build Test

--- lib/screens/onboarding_one.dart
+++ lib/screens/onboarding_one.dart
@@ -17,25 +17,55 @@ class _OnboardingOneState extends State<OnboardingOne> {
         color: Colors.white,
         child: Stack(
           children: [
-            Opacity(
-              opacity: 1,
-              child: ClipPath(
-                child: Container(
-                  height: screenHeight * 0.4, // 40% of screen height
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        Color(0xFF825FF5),
-                        Color(0xFFA677F2),
-                      ],
-                    ),
+            ClipPath(
+              clipper: WaveClipper(),
+              child: Container(
+                height: screenHeight * 0.5, // 40% of screen height
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color(0xFFE38FD8),
+                      Color(0xFFC457F2),
+                    ],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
                   ),
                 ),
               ),
             ),
+            Positioned(
+              top: screenHeight * 0.1,
+              child: Image.asset(
+                "assets/images/onboarding_one.png",
+                fit: BoxFit.contain,
+              ),
+            )
           ],
         ),
       ),
     );
   }
+}
+
+// Custom Clipper for Diagonal Wave
+class WaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+
+    path.lineTo(0, size.height * 0.8); // Start from bottom-left
+
+    path.quadraticBezierTo(
+      size.width * 0.6, size.height * 1.33, // Control point
+      size.width, size.height * 0.6, // End point
+    );
+
+    path.lineTo(size.width, 0); // Close at top-right
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }

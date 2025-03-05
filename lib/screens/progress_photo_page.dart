7aac8a41f7f4a1d0b34b399b6c19af9546754a18
itsPragadees <pragadeesofficialit@gmail.com>
2025-03-05 10:13:09 +0530

    Safety Commit before meet

---
+++ lib/screens/progress_photo_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProgressPhotoPage extends StatefulWidget {
+  const ProgressPhotoPage({super.key});
+
+  @override
+  State<ProgressPhotoPage> createState() => _ProgressPhotoPageState();
+}
+
+class _ProgressPhotoPageState extends State<ProgressPhotoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Progress Photos"),
+    );
+  }
+}

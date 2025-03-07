bc5c1994980d038c831c4748b70a373c661d2760
Kani-3004 <avvaikanimozhi@gmail.com>
2025-03-07 19:25:02 +0530

    progress photo page added

--- lib/screens/progress_photo_page.dart
+++ lib/screens/progress_photo_page.dart
@@ -1,3 +1,6 @@
+import 'package:fitness_mobile_app/widgets/compare_card_progress_photo.dart';
+import 'package:fitness_mobile_app/widgets/reminder_card_progress_photo_page.dart';
+import 'package:fitness_mobile_app/widgets/track_progress_card_progress_photo.dart';
 import 'package:flutter/material.dart';
 
 class ProgressPhotoPage extends StatefulWidget {
@@ -10,8 +13,204 @@ class ProgressPhotoPage extends StatefulWidget {
 class _ProgressPhotoPageState extends State<ProgressPhotoPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Progress Photos"),
+    final double screenHeight = MediaQuery.of(context).size.height;
+    // final double screenWidth = MediaQuery.of(context).size.width;
+
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 0.0,horizontal: 8.0),
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            centerTitle: true,
+            leading: Padding(
+              padding: EdgeInsets.only(
+                top: 16.0,
+                left: 16.0,
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color(0xffF7F8F8),
+                ),
+                child: IconButton(
+                  onPressed: () {
+                    // Navigator.of(context).pushReplacement(
+                    //   MaterialPageRoute(builder: (context) => HomePageTemplate()),
+                    // );
+                  },
+                  icon: Icon(Icons.arrow_back_ios_new_rounded, size: 15),
+                ),
+              ),
+            ),
+            title: Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: Text(
+                textAlign: TextAlign.center,
+                'Progress Photo',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0, right: 16.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xffF7F8F8),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset('assets/images/Detail-Navs.png'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          body: Padding(
+            padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+            child: Column(
+              children: [
+                ReminderCardProgressPhotoPage(),
+                SizedBox(height: 20,),
+                TrackProgressCardProgressPhoto(),
+                SizedBox(height: 25,),
+                CompareCardProgressPhoto(),
+                SizedBox(height: 5,),
+                Expanded(
+                  child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 16.0, horizontal: 12.0),
+                    child: Container(
+                      color: Colors.white,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Gallery',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: screenHeight * 0.02,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                              Text(
+                                'See more',
+                                style: TextStyle(
+                                    color: Color(0xffa5a3b0),
+                                    fontSize: screenHeight * 0.015,
+                                    fontWeight: FontWeight.w500),
+                              )
+                            ],
+                          ),
+                          Expanded(
+                            child: SingleChildScrollView(
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(vertical: 16.0),
+                                    child: Align(
+                                      alignment: Alignment.topLeft,
+                                      child: Text(
+                                        '2 June',
+                                        style: TextStyle(
+                                            color: Color(0xffb6b4c2),
+                                            fontSize: screenHeight * 0.015,
+                                            fontWeight: FontWeight.w400),
+                                      ),
+                                    ),
+                                  ),
+                                  SingleChildScrollView(
+                                    scrollDirection: Axis.horizontal,
+                                    child: Row(
+                                      children: [
+                                        Padding(
+                                          padding: EdgeInsets.symmetric(horizontal: 4.0),
+                                          child: Image.asset(
+                                              'assets/images/Rectangle 5834.png'),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.symmetric(horizontal: 4.0),
+                                          child: Image.asset(
+                                              'assets/images/Rectangle 5836.png'),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.symmetric(horizontal: 4.0),
+                                          child: Image.asset(
+                                              'assets/images/Rectangle 5835.png'),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.symmetric(horizontal: 4.0),
+                                          child: Image.asset(
+                                              'assets/images/Rectangle 5838.png'),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(vertical: 16.0),
+                                    child: Align(
+                                      alignment: Alignment.topLeft,
+                                      child: Text(
+                                        '5 May',
+                                        style: TextStyle(
+                                            color: Color(0xffb6b4c2),
+                                            fontSize: screenHeight * 0.015,
+                                            fontWeight: FontWeight.w400),
+                                      ),
+                                    ),
+                                  ),
+                                  SingleChildScrollView(
+                                    scrollDirection: Axis.horizontal,
+                                    child: Row(
+                                      children: [
+                                        Padding(
+                                          padding: EdgeInsets.symmetric(horizontal: 4.0),
+                                          child: Image.asset(
+                                              'assets/images/Rectangle 5838.png'),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.symmetric(horizontal: 4.0),
+                                          child: Image.asset(
+                                              'assets/images/Rectangle 5841.png'),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.symmetric(horizontal: 4.0),
+                                          child: Image.asset(
+                                              'assets/images/Rectangle 5839.png'),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.symmetric(horizontal: 4.0),
+                                          child: Image.asset(
+                                              'assets/images/Rectangle 5834.png'),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/track_progress_card_progress_photo.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+class TrackProgressCardProgressPhoto extends StatefulWidget {
+  const TrackProgressCardProgressPhoto({super.key});
+
+  @override
+  State<TrackProgressCardProgressPhoto> createState() =>
+      _TrackProgressCardProgressPhotoState();
+}
+
+class _TrackProgressCardProgressPhotoState
+    extends State<TrackProgressCardProgressPhoto> {
+  @override
+  Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
+    return Card(
+      color: Color(0xffF6E3FA),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ShaderMask(
+                      shaderCallback: (bounds) => LinearGradient(
+                              colors: [Color(0xffeea4ce), Color(0xffc150f6)],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight)
+                          .createShader(bounds),
+                      child: RichText(
+                        text: TextSpan(
+                            text: 'Track Your Progress Each \nMonth With ',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: screenHeight * 0.016,
+                                height: 1.5,
+                                fontWeight: FontWeight.w500),
+                            children: [
+                              TextSpan(
+                                  text: 'Photo',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: screenHeight * 0.016,
+                                      height: 1.5,
+                                      fontWeight: FontWeight.w600))
+                            ]),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        // Navigator.pushReplacement(
+                        //     context,
+                        //     MaterialPageRoute(
+                        //         builder: (BuildContext context) =>
+                        //             OnboardingOne()));
+                      },
+                      child: Container(
+                        width: screenWidth * 0.27,
+                        height: screenHeight * 0.045,
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                                colors: [
+                                  Color(0xff6b50f6),
+                                  Color(0xffcc8fed)
+                                ],
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight),
+                            borderRadius: BorderRadius.circular(40)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Learn more',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: screenHeight * 0.015,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Image.asset(
+                  'assets/images/progress-calender.png',
+                  height: screenHeight * 0.12,
+                  width: screenWidth * 0.26,
+                  fit: BoxFit.fill,
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
